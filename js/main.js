9b2920af40c7b730e61d8cd634459e4d1d35a979
Shadaab-Ahmed <asra.sultana17@gmail.com>
2025-11-23 07:53:20 -0800

    update

---
+++ js/main.js
@@ -0,0 +1,26 @@
+console.log("Hello World!");
+
+
+// Task 1: Full name
+function getFullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+console.log(getFullName("Asra", "Sultana"));
+
+// Task 2: Mean
+function findMean(numbers) {
+    let sum = 0;
+    for (let n of numbers) {
+        sum += n;
+    }
+    return sum / numbers.length;
+}
+console.log(findMean([20, 30, 40]));
+
+// Task 3: Right tree
+function printRightTree(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log(" ".repeat(i - 1) + i);
+    }
+}
+printRightTree(5);
